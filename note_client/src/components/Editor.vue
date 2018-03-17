d86c187f6272cfc080fbd49f97d6d7d42ea76ed5
Shinya Okano <tokibito@gmail.com>
2018-03-17 15:33:28 +0900

    polyfill

--- note_client/src/components/Editor.vue
+++ note_client/src/components/Editor.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="form-group">
   <input type="text" class="form-control" placeholder="タイトルを入力" v-model="page.title">
-  <textarea class="form-control" rows="20" placeholder="内容" v-model="page.content"></textarea>
+  <textarea class="form-control" rows="15" placeholder="内容" v-model="page.content"></textarea>
 </div>
 </template>
 
